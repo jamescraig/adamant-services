0808115109431a6dc10327088f0659af9163b628
James Craig <james.craig@gmail.com>
2018-09-27 15:08:48 -0400

    add scrollmagic back ready for ssr

--- src/components/animationBlock.js
+++ src/components/animationBlock.js
@@ -1,38 +1,28 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-//import ScrollMagic, { Scene } from 'ScrollMagic'
 import { TimelineMax } from 'gsap'
-//import 'debug.addIndicators'
-
-//const controller = new ScrollMagic.Controller()
 
 export default class ScrollMagicExample extends Component {
-  static contextTypes = {
-    scrollmagic: PropTypes.any,
-  }
-
-  static propTypes = {
-    children: PropTypes.any,
-  }
-
   componentDidMount() {
+    const isBrowser = typeof window !== 'undefined';
+    const ScrollMagic = isBrowser ? require( 'ScrollMagic') : undefined;
+    const controller = new ScrollMagic.Controller()
+    console.log(isBrowser)
+
     this.animation = new TimelineMax({ paused: true })
       .fromTo(this.node, 5, { opacity: 0 }, { opacity: 0.85 })
-    this.animation.play()
-    /*
+
     this.scrollAnim = new TimelineMax({ paused: true })
       .fromTo(this.title, 5, { opacity: 0, y: 50 }, { opacity: 1, y: 0 })
 
-    this.scene = new Scene({ triggerElement: this.title, triggerHook: 0.4 })
-    this.scene.indicatorName = 'Test'
-    this.scene.on('enter', () => this.animation.play(), this.scrollAnim.play())
+    const scene = new ScrollMagic.Scene({ triggerElement: this.title, triggerHook: 0.4 })
+    scene.indicatorName = 'Test'
+    scene.on('enter', () => this.animation.play(), this.scrollAnim.play())
 
     if (process.env.NODE_ENV === 'development') {
-      this.scene.addIndicators({ name: this.scene.indicatorName })
+      //this.scene.addIndicators({ name: this.scene.indicatorName })
     }
 
-    this.scene.addTo(controller)
-    */
+    scene.addTo(controller)
   }
 
   // Binding
